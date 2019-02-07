ee49f02ead1d99adb6e16c051afd25a9f907f027
Jackgwharton <38636074+Jackgwharton@users.noreply.github.com>
2019-02-07 11:33:53 -0800

    Added test cases for the FibonacciTextReader class

--- Cpts 321 HW3/Cpts 321 HW3/FibonacciTextReader.cs
+++ Cpts 321 HW3/Cpts 321 HW3/FibonacciTextReader.cs
@@ -13,12 +13,18 @@ namespace Cpts_321_HW3
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using NUnit.Framework;
 
     /// <summary>
     /// Class for loading fibonacci sequence
     /// </summary>
-    public class FibonacciTextReader
+    public class FibonacciTextReader : System.IO.TextReader
     {
+        private int prevNum1;
+        private int prevNum2;
+        private int lineNumber;
+        private int maxLines;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="FibonacciTextReader"/> class.
         /// </summary>
@@ -26,6 +32,53 @@ namespace Cpts_321_HW3
         public FibonacciTextReader(int maxLinesAvailable)
         {
 
+        }
+
+        /// <summary>
+        /// Calculates the next number in the fibonacci sequence based on the value of this.lineNumber
+        /// </summary>
+        /// <returns>The next number in the fibonacci sequence as a string</returns>
+        public string ReadLine()
+        {
+
+        }
+
+        /// <summary>
+        /// Calls ReadLine as many times as specified by this.maxLines, and concatenates results to a single string
+        /// </summary>
+        /// <returns>a string </returns>
+        public string ReadToEnd()
+        {
+
+        }
+
+        /// <summary>
+        /// Tests the ability of ReadLine to generate the first 10 elements of the fibonacci sequence
+        /// </summary>
+        [Test]
+        public void First10_FibonacciTest()
+        {
+            Assert.AreEqual(int.Parse(this.ReadLine()), 0);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 1);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 1);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 2);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 3);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 5);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 8);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 13);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 21);
+            Assert.AreEqual(int.Parse(this.ReadLine()), 34);
+        }
+
+        /// <summary>
+        /// Verifies that the number of fibonacci items returned by ReadToEnd matches the given max lines
+        /// </summary>
+        [Test]
+        public void TotalLinesTest()
+        {
+            this.maxLines = 10;
+            this.ReadToEnd();
+            Assert.AreEqual(this.lineNumber, this.maxLines);
         }
     }
 }
