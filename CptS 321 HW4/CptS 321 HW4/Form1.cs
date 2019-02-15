bd171332877c5cc720dcb3b32a03573d6641a846
Jackgwharton <38636074+Jackgwharton@users.noreply.github.com>
2019-02-15 11:16:27 -0800

    Implemented initialization of grid rows and columns

--- CptS 321 HW4/CptS 321 HW4/Form1.cs
+++ CptS 321 HW4/CptS 321 HW4/Form1.cs
@@ -1,20 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CptS_321_HW4
+﻿namespace CptS_321_HW4
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+            char letter = 'A';
+
+            this.InitializeComponent();
+
+            /* Resize form window */
+            this.Size = new System.Drawing.Size(800, 500);
+
+            /* Create columns A through Z */
+            for (letter = 'A'; letter != '['; letter++)
+            {
+                this.dataGridView1.Columns.Add(letter.ToString(), letter.ToString());
+            }
+
+            /* Create rows 1 through 50 */
+            this.dataGridView1.Rows.Add(50);
+            this.dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            foreach (DataGridViewRow row in this.dataGridView1.Rows)
+            {
+                row.HeaderCell.Value = (row.Index + 1).ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
