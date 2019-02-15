f69441f9928c1039723bab99671e485855734f2d
Jackgwharton <38636074+Jackgwharton@users.noreply.github.com>
2019-02-15 12:58:41 -0800

    Added framework for Cell and Sheet classes

---
+++ CptS 321 HW4/SpreadsheetEngine/Cell.cs
@@ -0,0 +1,43 @@
+﻿namespace CptS321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class Cell
+    {
+        private int columnIndex;
+        private int rowIndex;
+        protected string text;
+        protected string value;
+
+        public Cell()
+        {
+
+        }
+
+        public int ColumnIndex
+        {
+            get { return this.columnIndex; }
+        }
+
+        public int RowIndex
+        {
+            get { return this.rowIndex; }
+        }
+
+        public string Text
+        {
+            get { return this.text; }
+            set { /* if text changed to a different value, fire property changed event */}
+        }
+
+        public string Value
+        {
+            get { return this.value; }
+            // only spreadsheet should be allowed to change this value
+        }
+    }
+}
---
+++ CptS 321 HW4/SpreadsheetEngine/Sheet.cs
@@ -0,0 +1,35 @@
+﻿namespace CptS321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Sheet
+    {
+        private Cell[,] cells;
+        private int columnCount;
+        private int rowCount;
+
+        Sheet(int columns, int rows)
+        {
+            cells = new Cell[columns, rows];
+        }
+
+        public int ColumnCount
+        {
+            get { return columnCount; }
+        }
+
+        public int RowCount
+        {
+            get { return rowCount; }
+        }
+
+        public Cell GetCell(int column, int row)
+        {
+            return null;
+        }
+    }
+}
