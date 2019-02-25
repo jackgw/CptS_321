454a31ff8936adf976036bc7b362ebeb30801f58
Jackgwharton <38636074+Jackgwharton@users.noreply.github.com>
2019-02-25 12:33:00 -0800

    Added XML comments to classes and functions

---
+++ ExpressionTreeApp/ExpressionTreeApp/App.cs
@@ -0,0 +1,23 @@
+﻿namespace ExpressionTreeApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Console applicaion for interation with the ExpressionTree class.
+    /// </summary>
+    public class App
+    {
+        /// <summary>
+        /// Prompts the user for an expression, and instantiates an ExpressionTree using the expression.
+        /// Provides options to set variable value, evaluate, or quit.
+        /// </summary>
+        /// <param name="args">External string arguments</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ExpressionTreeApp/SpreadsheetEngine/ExpressionTree.cs
@@ -0,0 +1,42 @@
+﻿namespace CptS321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///  Implements an arithmetic expression parser that builds a tree for a given expression.
+    ///  The tree is then used for evaluation of the expression.
+    /// </summary>
+    class ExpressionTree
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionTree"/> class.
+        /// Constructs the tree from a specific expression
+        /// </summary>
+        /// <param name="expression">Expression to be parsed</param>
+        public ExpressionTree(string expression)
+        {
+
+        }
+
+        /// <summary>
+        /// Sets the specified variable within the ExpressionTree variables dictionary.
+        /// </summary>
+        /// <param name="variableName">Name of target variable</param>
+        /// <param name="variableValue">Desired variable value</param>
+        public void SetVariable(string variableName, double variableValue)        {
+
+        }
+
+        /// <summary>
+        /// Evaluates the tree expression to a double value.
+        /// </summary>
+        /// <returns>Value of the expression</returns>
+        public double Evaluate()        {
+
+        }
+    }
+}
